a99ef9e93866c9361d173c7d9fd56267fd3ed579
Ahmadreza Dorkhah <djahmadfire@gmail.com>
2024-07-28 10:28:52 +0330

    force multi threaded

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,7 @@
     windows_subsystem = "windows"
 )]
 
-use std::{path::PathBuf, sync::{Arc, Mutex}};
+use std::{num::NonZero, path::PathBuf, sync::{Arc, Mutex}};
 
 use id3::{Tag, TagLike};
 use tauri::api::path::home_dir;
@@ -109,107 +109,69 @@ fn cover_caching() -> Vec<SongMeta> {
         dirs = dirs_temp;
     }
 
-    if let Ok(cpu) = std::thread::available_parallelism() {
-        let arc_songs_list = Arc::new(songs_list);
-        let config: Arc<Mutex<Vec<SongMeta>>> = Arc::new(Mutex::new(Vec::new()));
-        let couner = Arc::new(Mutex::new(1));
-        let mut handlers = Vec::new();
-        let share = arc_songs_list.len()/cpu.get();
-        for thread_n in 0..cpu.get() {
-            let arc_songs_list = arc_songs_list.clone();
-            let config = config.clone();
-            let couner = couner.clone();
-            let temp = temp.clone();
-            handlers.push(
-                std::thread::spawn(move||{
-                    let songs_list = {
-                        if thread_n+1==cpu.get(){
-                            &arc_songs_list[(share*thread_n)..]
-                        }else {
-                            &arc_songs_list[(share*thread_n)..(share*(thread_n+1))]
-                        }
-                    };
-                    for song_f in songs_list{
-                        let nameer = song_f.clone();
-                        let cover_name = nameer.file_name().unwrap().to_str().unwrap().replace(".mp3", ".jpg");
-                        if let Ok(s) = Tag::read_from_path(&nameer) {
-                            config.lock().unwrap().push(SongMeta {
-                                path: song_f.clone(),
-                                artist: s.artist().unwrap_or("").to_string(),
-                                title: s
-                                    .title()
-                                    .unwrap_or(&song_f.to_str().unwrap().split("\\").last().unwrap())
-                                    .to_string(),
-                                genre: s.genre().unwrap_or("").to_string(),
-                                date: s.date_recorded().unwrap_or_default().to_string(),
-                                duration: s.duration().unwrap_or_default(),
-                                no: *couner.lock().unwrap(),
-                                lyric: s.lyrics().map(|ly| ly.to_string()).collect::<Vec<String>>(),
-                                cover: cover_ceck(temp.join(format!("{}", &cover_name))),
-                                modified: std::fs::metadata(&song_f).unwrap().created().unwrap(),
-                            });
-                            *couner.lock().unwrap() += 1;
-                            // save pictures to temp folders
-                            for pic in s.pictures() {
-                                if !temp.join(&cover_name).exists() {
-                                    match image::load_from_memory(&pic.data) {
-                                        Ok(ready) => {
-                                            let resized = ready.resize(128, 128, image::imageops::Nearest);
-                                            resized.save(&temp.join(format!("{}", cover_name))).unwrap();
-                                        }
-                                        Err(_) => (),
+    let cpu = std::thread::available_parallelism().unwrap_or(NonZero::new(2).unwrap());
+    let arc_songs_list = Arc::new(songs_list);
+    let config: Arc<Mutex<Vec<SongMeta>>> = Arc::new(Mutex::new(Vec::new()));
+    let couner = Arc::new(Mutex::new(1));
+    let mut handlers = Vec::new();
+    let share = arc_songs_list.len()/cpu.get();
+    for thread_n in 0..cpu.get() {
+        let arc_songs_list = arc_songs_list.clone();
+        let config = config.clone();
+        let couner = couner.clone();
+        let temp = temp.clone();
+        handlers.push(
+            std::thread::spawn(move||{
+                let songs_list = {
+                    if thread_n+1==cpu.get(){
+                        &arc_songs_list[(share*thread_n)..]
+                    }else {
+                        &arc_songs_list[(share*thread_n)..(share*(thread_n+1))]
+                    }
+                };
+                for song_f in songs_list{
+                    let nameer = song_f.clone();
+                    let cover_name = nameer.file_name().unwrap().to_str().unwrap().replace(".mp3", ".jpg");
+                    if let Ok(s) = Tag::read_from_path(&nameer) {
+                        config.lock().unwrap().push(SongMeta {
+                            path: song_f.clone(),
+                            artist: s.artist().unwrap_or("").to_string(),
+                            title: s
+                                .title()
+                                .unwrap_or(&song_f.to_str().unwrap().split("\\").last().unwrap())
+                                .to_string(),
+                            genre: s.genre().unwrap_or("").to_string(),
+                            date: s.date_recorded().unwrap_or_default().to_string(),
+                            duration: s.duration().unwrap_or_default(),
+                            no: *couner.lock().unwrap(),
+                            lyric: s.lyrics().map(|ly| ly.to_string()).collect::<Vec<String>>(),
+                            cover: cover_ceck(temp.join(format!("{}", &cover_name))),
+                            modified: std::fs::metadata(&song_f).unwrap().created().unwrap(),
+                        });
+                        *couner.lock().unwrap() += 1;
+                        // save pictures to temp folders
+                        for pic in s.pictures() {
+                            if !temp.join(&cover_name).exists() {
+                                match image::load_from_memory(&pic.data) {
+                                    Ok(ready) => {
+                                        let resized = ready.resize(128, 128, image::imageops::Nearest);
+                                        resized.save(&temp.join(format!("{}", cover_name))).unwrap();
                                     }
+                                    Err(_) => (),
                                 }
                             }
                         }
                     }
-                })
-            )
-        }
-        for handler in handlers {
-            handler.join().unwrap();
-        }
-        let c = config.lock().unwrap().clone();
-        c
-    } else {
-        let mut config: Vec<SongMeta> = Vec::new();
-        let mut couner: usize = 1;
-        for song_f in songs_list {
-            let nameer = song_f.clone();
-            let cover_name = nameer.file_name().unwrap().to_str().unwrap().replace(".mp3", ".jpg");
-            if let Ok(s) = Tag::read_from_path(&nameer) {
-                config.push(SongMeta {
-                    path: song_f.clone(),
-                    artist: s.artist().unwrap_or("").to_string(),
-                    title: s
-                        .title()
-                        .unwrap_or(&song_f.to_str().unwrap().split("\\").last().unwrap())
-                        .to_string(),
-                    genre: s.genre().unwrap_or("").to_string(),
-                    date: s.date_recorded().unwrap_or_default().to_string(),
-                    duration: s.duration().unwrap_or_default(),
-                    no: couner,
-                    lyric: s.lyrics().map(|ly| ly.to_string()).collect::<Vec<String>>(),
-                    cover: cover_ceck(temp.join(format!("{}", &cover_name))),
-                    modified: std::fs::metadata(&song_f).unwrap().created().unwrap(),
-                });
-                couner += 1;
-                // save pictures to temp folders
-                for pic in s.pictures() {
-                    if !temp.join(&cover_name).exists() {
-                        match image::load_from_memory(&pic.data) {
-                            Ok(ready) => {
-                                let resized = ready.resize(128, 128, image::imageops::Nearest);
-                                resized.save(&temp.join(format!("{}", cover_name))).unwrap();
-                            }
-                            Err(_) => (),
-                        }
-                    }
                 }
-            }
-        }
-        config
+            })
+        )
     }
+    for handler in handlers {
+        handler.join().unwrap();
+    }
+
+    let c = config.lock().unwrap().to_owned();
+    c
 }
 
 #[tauri::command]
